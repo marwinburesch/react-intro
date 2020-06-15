47e338e8d6007b4613e26eb14a3ac6678aebb18d
marwinburesch <marwin@posteo.de>
2020-06-15 16:01:11 +0200

    Add Header display name based on what content is showing

--- src/App.js
+++ src/App.js
@@ -5,8 +5,14 @@ import Footer from "./components/Footer";
 import MainContent from "./components/MainContent";
 import "./App.css";
 
-const beers = ["Punk IPA", "Hoevels", "Chainbreaker", "Elvis Juice"];
-const animals = ["Sloth", "Ant", "Elephant", "Elvis"];
+const beers = {
+  name: "beers",
+  items: ["Punk IPA", "Hoevels", "Chainbreaker", "Elvis Juice"],
+};
+const animals = {
+  name: "animals",
+  items: ["Sloth", "Ant", "Elephant", "Elvis"],
+};
 
 export default function App() {
   const [itemsToDisplay, setItemsToDisplay] = React.useState(beers);
@@ -21,11 +27,11 @@ export default function App() {
 
   return (
     <div className="App">
-      <Header>Marwin's favourite beers</Header>
+      <Header>Marwin's favourite {itemsToDisplay.name} </Header>
       <MainContent>
         <button onClick={displayBeers}>show me beers!</button>
         <button onClick={displayAnimals}>show me animals!</button>
-        <List items={itemsToDisplay} />
+        <List items={itemsToDisplay.items} />
       </MainContent>
       <Footer>made with React</Footer>
     </div>
